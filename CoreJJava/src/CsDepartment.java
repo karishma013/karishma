2ec3fad6d38fde443cfa8a27b9358bd1d60a93c6
karis <karishma884@gmail.com>
2023-06-04 21:34:07 -0500

    practice

---
+++ CoreJJava/src/CsDepartment.java
@@ -0,0 +1,15 @@
+
+public class CsDepartment extends Department{
+
+		
+		public int DepartementSize(int n) {
+			return n;					
+		}
+		public static void main(String[] args) {
+			Department dep=new CsDepartment();
+			dep.show();
+			System.out.println(+dep.DepartementSize(10) );
+			
+			
+}
+}
